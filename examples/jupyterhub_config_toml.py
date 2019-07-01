19b25186c441e2d030e2f53069ec52f2eeae2e31
GeorgianaElena <georgiana.dolocan@gmail.com>
2019-07-01 16:00:09 +0300

    Make trefik's log level configurable

--- examples/jupyterhub_config_toml.py
+++ examples/jupyterhub_config_toml.py
@@ -11,6 +11,7 @@ requires jupyterhub 1.0.dev
 c.JupyterHub.proxy_class = "traefik_toml"
 c.TraefikTomlProxy.traefik_api_username = "admin"
 c.TraefikTomlProxy.traefik_api_password = "admin"
+c.TraefikTomlProxy.traefik_log_level = "INFO"
 
 # use dummy and simple auth/spawner for testing
 c.JupyterHub.authenticator_class = "dummy"
